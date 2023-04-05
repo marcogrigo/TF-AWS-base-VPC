abe5ba044eb744daa76edf8d5abc0c1461dd91e9
marcogrigo <me@marcogrigo.me>
2023-04-05 18:39:06 +0100

    add public subnets

--- networking/main.tf
+++ networking/main.tf
@@ -17,10 +17,21 @@ resource "aws_subnet" "private_subnet" {
   count                   = length(var.private_cidrs)
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_cidrs[count.index]
-  map_public_ip_on_launch = true
   availability_zone       = var.availability_zones[count.index]
 
   tags = {
     Name = "private_${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public_cidrs)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zones[count.index]
+
+  tags = {
+    Name = "public_${count.index + 1}"
   }
 }
